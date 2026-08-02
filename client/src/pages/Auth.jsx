499ebb073c35bf32d61af4c436bf4815c8d2679b
kunal1238 <kunal20230212@gmail.com>
2026-08-03 00:36:57 +0530

    Fix import path for Firebase authentication

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -3,7 +3,7 @@ import { BsRobot } from "react-icons/bs";
 import { IoSparkles } from "react-icons/io5";
 import { motion } from "motion/react"
 import { FcGoogle } from "react-icons/fc";
-import { signInWithPopup } from 'firebase/Auth';
+import { signInWithPopup } from 'firebase/auth';
 import { auth, provider } from '../utils/firebase';
 import axios from 'axios';
 import { ServerUrl } from '../App';
